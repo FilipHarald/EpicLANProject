2f65f6cadc374b4c26941d124474553dc345bff3
FilipHarald <Filip.harald@gmail.com>
2014-02-22 17:24:31 +0100

    Meh.

---
+++ EpicLANProject/src/EpicLANProjectApplication.java
@@ -0,0 +1,8 @@
+
+public class EpicLANProjectApplication {
+	public static void main(String[] args) {
+		RankingTable rankingTable = new RankingTable();
+	    new EpicLANProjectView(rankingTable);
+	}
+
+}
