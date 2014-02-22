20f1dc45d982315a01e661bd8de706835024a25c
FilipHarald <Filip.harald@gmail.com>
2014-02-22 15:20:10 +0100

    Initial Player class

---
+++ EpicLANProject/src/Player.java
@@ -0,0 +1,35 @@
+public class Player {
+
+	private String userName;
+	private int rating;
+	private int userID;
+
+	public Player(String userName, int userID) {
+		setUserName(userName);
+		setRating(1500);
+		this.userID = userID;
+	}
+
+	public Player(String userName, int userID, int rank) {
+		setUserName(userName);
+		setRating(1500);
+		this.userID = userID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getRank() {
+		return rating;
+	}
+
+}
